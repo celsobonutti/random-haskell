fbb1a7c94b6199badaffb5c98544548287540bee
Celso Bonutti <i.am@cel.so>
2022-11-07 10:50:15 -0300

    tuples

--- src/AssociatedTypes.hs
+++ src/AssociatedTypes.hs
@@ -5,12 +5,6 @@ import Prelude hiding (Functor, Applicative, Monad, pure, (>>=), (<*>), fmap, (<
 type family Wrap f b
 type family Unwrap f
 
-type instance Wrap (Maybe a) b = Maybe b
-type instance Unwrap (Maybe a) = a
-
-type instance Wrap [a] b = [b]
-type instance Unwrap [a] = a
-
 class Functor f where
   fmap :: (Unwrap f -> b) -> f -> Wrap f b
   fmap = (<$>)
@@ -31,6 +25,11 @@ class Applicative m => Monad m where
   (>>=) :: m -> (Unwrap m -> Wrap m b) -> Wrap m b
   {-# MINIMAL (>>=) #-}
 
+-- Functor/Applicative/Monad for Maybe
+
+type instance Wrap (Maybe a) b = Maybe b
+type instance Unwrap (Maybe a) = a
+
 instance Functor (Maybe a)  where
   fmap :: (a -> b) -> Maybe a -> Maybe b
   fmap f = \case
@@ -46,6 +45,11 @@ instance Applicative (Maybe a) where
 instance Monad (Maybe a) where
   Just x >>= f = f x
   Nothing >>= _ = Nothing
+
+-- Functor/Applicative/Monad for lists
+
+type instance Wrap [a] b = [b]
+type instance Unwrap [a] = a
 
 instance Functor [a] where
   fmap :: (a -> b) -> [a] -> [b]
@@ -65,10 +69,6 @@ type family UnwrapFirst f
 type family UnwrapSecond f
 type family BiWrap f a b
 
-type instance UnwrapFirst (Either a b) = a
-type instance UnwrapSecond (Either a b) = b
-type instance BiWrap (Either a b) c d = Either c d
-
 class Bifunctor f where
   bimap :: (UnwrapFirst f -> c) -> (UnwrapSecond f -> d) -> f -> BiWrap f c d
 
@@ -80,8 +80,24 @@ class Bifunctor f where
 
   {-# MINIMAL bimap #-}
 
+-- Bifunctor for Either
+
+type instance UnwrapFirst (Either a b) = a
+type instance UnwrapSecond (Either a b) = b
+type instance BiWrap (Either a b) c d = Either c d
+
 instance Bifunctor (Either a b) where
   bimap :: (a -> c) -> (b -> d) -> Either a b -> Either c d
   bimap f g = \case
     Left x -> Left $ f x
     Right y -> Right $ g y
+
+-- Bifunctor for tuple
+
+type instance UnwrapFirst (a, b) = a
+type instance UnwrapSecond (a, b) = b
+type instance BiWrap (a, b) c d = (c, d)
+
+instance Bifunctor (a, b) where
+  bimap :: (a -> c) -> (b -> d) -> (a, b) -> (c, d)
+  bimap f g (x, y) = (f x, g y)
